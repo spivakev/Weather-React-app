1f8824aaa0a7a95c69411777a1c4f84125706d65
Elena Spivak <spivak.e.v@gmail.com>
2020-10-06 21:38:26 +0300

    feat: add form to the Form component

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -3,7 +3,12 @@ import React from 'react';
 class Form extends React.Component {
   render() {
     return (
-      <div>Компонент с формой</div>
+      <div>
+        <form onSubmit={this.props.weatherMethod}>
+          <input type="text" name="city" placeholder="Город" />
+          <button>Получить погоду</button>
+        </form>
+      </div>
     );
   }
 }
